042fae6f85f0b0b2b6029a955c0e34b6d63d7336
Seolyoona <144825458+foryoonas@users.noreply.github.com>
2024-03-13 10:51:50 +0900

    Add files via upload

---
+++ week03/zuneee.js
@@ -0,0 +1,11 @@
+function agecalc() {
+    var x= str, result;
+    result=21;
+
+    if(result===21) {
+        var str='Correct, My age is ';
+        alert(str+result);
+    } else {
+        alert ('Try Again');
+    }
+}
